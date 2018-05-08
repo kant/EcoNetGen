ded7ddd6b6f926783620a016c15974cf152c115c
Carl Boettiger <cboettig@gmail.com>
2018-05-07 22:47:03 -0700

    first go at a cleaner api

--- R/NetGen.R
+++ R/NetGen.R
@@ -1,5 +1,84 @@
 #' netgen
 #'
+#' Randomly generate a wide range of interaction networks
+#'
+#' @param net_size network size (number of nodes)
+#' @param average_module average module size
+#' @param min_module cutoff for the minimum modules size
+#' @param min_submodule cutoff for submodules, used only for bipartite and tripartite networks
+#' @param net_type network type, see details
+#' @param net_degree average degree of connection
+#' @param net_rewire global and local  network rewiring probabilities
+#' @param mod_probs module probabilities for first 7 types,
+#'   used for constructing mixed networks
+#' @param verbose logical, default TRUE. Should a message report summary statistics?
+#' @details
+#' network type is one of
+#' - mixed
+#' - random
+#' - scalefree
+#' - nested
+#' - bi-partite nested
+#' - bi-partite random
+#' - "ttbnr": tri-trophic bipartite nested-random
+#' - "ttbnbn": tri-trophic bipartite nested-bipartite nested
+#'
+#'  NOTE: Function arguments have changed from those netgen 0.1.1 to be more intelligible.
+#'  To restore the original function api on code that depends on the old version, you
+#'  can simply add:
+#'
+#'  `netgen <- EcoNetGen:::netgen_v1`
+#'
+#'  to the top of your code after running `library(EcoNetGen)`.
+#'
+#' @importFrom igraph graph.data.frame graph_from_adjacency_matrix
+#' @importFrom utils read.table
+#' @return an `igraph` object
+#' @export
+#' @useDynLib EcoNetGen, .registration = TRUE
+#' @examples
+#' \donttest{
+#' net <- netgen()
+#' adj_plot(net)
+#' }
+netgen <- function(net_size = 50,
+                   average_module = 10,
+                   min_module = 0,
+                   min_submodule = 0,
+                   net_type = c("mixed",
+                                "random",
+                                "scalefree",
+                                "nested",
+                                "bi-partite nested",
+                                "bi-partite random",
+                                "tt-bn-r",
+                                "tt-bn-bn"
+                                ),
+                   net_degree = 10,
+                   net_rewire = c(0.3,0.0),
+                   mod_probs = 0,
+                   verbose = TRUE){
+
+  net_type <- match.arg(net_type)
+  net_type <- switch(net_type,
+                     "mixed" = 0,
+                     "random" = 1,
+                     "scalefree" = 2,
+                     "nested" = 3,
+                     "bi-partite nested" = 41,
+                     "bi-partite random" = 42,
+                     "tt-bn-r" = 51,
+                     "tt-bn-bn" = 52)
+  n_modav = c(net_size, average_module)
+  cutoffs = c(min_module, min_submodule)
+
+  netgen_v1(n_modav, cutoffs, net_type, net_degree, net_rewire, mod_probs)
+
+}
+
+
+#' netgen_v1
+#'
 #' netgen function
 #'
 #' @param n_modav network size and average module size (integer vector, length 2)
@@ -19,14 +98,13 @@
 #' - 3 = nested
 #' - 41 = bi-partite nested
 #' - 42 = bi-partite random
-#' - 51 = tri-trophic bipartite nested-random
-#' - 52 = tri-trophic bipartite nested-bipartite nested
+#' - 51 = tri-trophic bipartite nested-random "ttbnr"
+#' - 52 = tri-trophic bipartite nested-bipartite nested "ttbnbn"
 #' @importFrom igraph graph.data.frame graph_from_adjacency_matrix
 #' @importFrom utils read.table
 #' @return an `igraph` object
 #' @useDynLib EcoNetGen, .registration = TRUE
-#' @export
-netgen <-
+netgen_v1 <-
   function(
            n_modav = c(50, 10),
            cutoffs = c(3, 0),
--- tests/testthat/test-netgen.R
+++ tests/testthat/test-netgen.R
@@ -1,6 +1,13 @@
 library(testthat)
 testthat::context("netgen")
 
+
+test_that("we can create a random type network", {
+  net <- netgen()
+  expect_is(net, "igraph")
+
+})
+
 test_that("we can create a mixed type network", {
 n_modav <- c(150,30)
 cutoffs <- c(15,5)
@@ -9,7 +16,7 @@ net_degree <- 10.0
 net_rewire <- c(0.07,0.2)
 mod_probs <- c(0.2, 0.0, 0.3, 0.3, 0.2, 0.0, 0.0)
 
-M <- netgen(n_modav,cutoffs,net_type,net_degree,net_rewire,mod_probs)
+M <- netgen_v1(n_modav,cutoffs,net_type,net_degree,net_rewire,mod_probs)
 
 expect_is(M, "igraph")
 p1 <- adj_plot(M)
@@ -25,19 +32,19 @@ test_that("we can create all types of network", {
         net_rewire <- c(0.07,0.2)
         mod_probs <- c(0.2, 0.0, 0.3, 0.3, 0.2, 0.0, 0.0)
 
-        M <- netgen(n_modav,cutoffs,1,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,1,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
-        M <- netgen(n_modav,cutoffs,2,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,2,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
-        M <- netgen(n_modav,cutoffs,3,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,3,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
-        M <- netgen(n_modav,cutoffs,41,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,41,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
-        M <- netgen(n_modav,cutoffs,51,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,51,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
-        M <- netgen(n_modav,cutoffs,42,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,42,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
-        M <- netgen(n_modav,cutoffs,52,net_degree,net_rewire,mod_probs)
+        M <- netgen_v1(n_modav,cutoffs,52,net_degree,net_rewire,mod_probs)
         expect_is(M, "igraph")
 
 })
