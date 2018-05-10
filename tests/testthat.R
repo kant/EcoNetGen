41150605f74cb85e2af3a392e896490b566103fd
Carl Boettiger <cboettig@gmail.com>
2018-05-10 09:29:53 -0700

    ggraph using 'kk' layout okay with 32bit windoze?

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,9 +3,9 @@ library(EcoNetGen)
 
 ## Windows i386 builds always time out when ggraph
 ## circle layout is present in README images.  CRAZY
-skip <- (R.Version()$arch == "i386") &
-  "windows" %in% tolower(Sys.info()[["sysname"]])
+#skip <- (R.Version()$arch == "i386") &
+#  "windows" %in% tolower(Sys.info()[["sysname"]])
 
-if(!skip){
-  test_check("EcoNetGen")
-}
+#if(!skip){
+test_check("EcoNetGen")
+#}
